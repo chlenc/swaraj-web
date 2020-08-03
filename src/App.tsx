ea973f28e939735e282ccf7182c4f10f444c46bd
Applegrub <sidyukovmichail@list.ru>
2020-08-03 17:25:20 +0300

    base

--- src/App.tsx
+++ src/App.tsx
@@ -5,64 +5,15 @@ import {ROUTES} from './ROUTES'
 import MainPage from './components/body/mainPage/MainPage'
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import About from "./components/body/topMenu/About/About";
-import FAQ from "./components/body/topMenu/FAQ/FAQ";
-import Dialog from "./components/Dialog";
-import Blog from "./components/body/topMenu/blog/Blog";
-import Docs from "./components/body/topMenu/docs/Docs";
-import AuthenticateAnItem from "./components/body/buttomMenu/authenticateAnItem/AuthenticateAnItem";
-import PrivacyPolicy from "./components/body/buttomMenu/privacyPolicy/PrivacyPolicy";
-import TermsOfService from "./components/body/buttomMenu/termsOfService/TermsOfService";
-import HowToShop from "./components/body/buttomMenu/customerCare/HowToShop";
-import ProductInfo from "./components/body/buttomMenu/customerCare/ProductInfo";
-import Payment from "./components/body/buttomMenu/customerCare/Payment";
-import Delivery from "./components/body/buttomMenu/customerCare/Delivery";
-import Returns from "./components/body/buttomMenu/customerCare/Returns";
-import SignIn from "./components/body/mainPage/dialogs/SignIn";
-import CreateAccount from "./components/body/mainPage/dialogs/CreateAccount";
-import GoodCard from "./components/body/mainPage/dialogs/GoodCard";
-import NewAddress from "./components/body/mainPage/dialogs/NewAddress";
-import MyThings from "./components/body/myWardrobe/myThings/MyThings";
-import ViewHistory from "./components/body/myWardrobe/viewHistory/ViewHistory";
-import ViewWallet from "./components/body/myWardrobe/ViewWallet";
-import Settings from "./components/body/myWardrobe/Settings";
 import Admin from "./components/body/myWardrobe/Admin/Admin";
-import Send from "./components/body/myWardrobe/dialogs/Send";
-import Sent from "./components/body/myWardrobe/dialogs/Sent";
-
 
 const history = createBrowserHistory()
 
 const App: React.FC = () => <Router history={history}>
             <Header/>
             <Switch>
-                <Route exact path={ROUTES.ROOT} component={MainPage}/>
-                <Route path={ROUTES.FAQ} component={FAQ}/>
-                <Route path={ROUTES.ABOUT} component={About}/>
-                <Route exact path={ROUTES.BLOG} component={Blog}/>
-                <Route exact path={ROUTES.DOCS} component={Docs}/>
-                <Route exact path={ROUTES.PRIVACYPOLICY} component={PrivacyPolicy}/>
-                <Route exact path={ROUTES.TERMSofSERVICE} component={TermsOfService}/>
-                <Route exact path={ROUTES.AUTHENTICATEanITEM} component={AuthenticateAnItem}/>
-
-                <Route path={ROUTES.HOWtoSHOP} component={HowToShop}/>
-                <Route path={ROUTES.PRODUCTINFO} component={ProductInfo}/>
-                <Route path={ROUTES.PAYMENT} component={Payment}/>
-                <Route path={ROUTES.DELIVERY} component={Delivery}/>
-                <Route path={ROUTES.RETURNS} component={Returns}/>
-
-                <Route path={ROUTES.MYTHINGS} component={MyThings}/>
-                <Route path={ROUTES.VIEWHISTORY} component={ViewHistory}/>
-                <Route path={ROUTES.VIEWWALLET} component={ViewWallet}/>
-                <Route path={ROUTES.SETTINGS} component={Settings}/>
                 <Route path={ROUTES.ADMIN} component={Admin}/>
             </Switch>
-
-            <Route path={ROUTES.SIGNIN} component={SignIn}/>
-            <Route path={ROUTES.INSTALLMETAMASK} component={CreateAccount}/>
-            <Route path={ROUTES.BUY} component={GoodCard}/>
-            <Route path={ROUTES.SEND} component={Send}/>
-            <Route path={ROUTES.SENT} component={Sent}/>
 
             <Footer/>
         </Router>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import MenuButton from "../MenuButton";
-import logo from '../../assets/images/logo.svg'
 import {ROUTES} from "../../ROUTES";
-import {Link} from "react-router-dom";
-import Button from "../Button";
 
 const Root = styled.div`
 display: flex;
@@ -15,31 +12,11 @@ line-height: 114%;
     text-transform: capitalize;
     color: #4A4B57;
 `
-const Menu = styled.div`
-padding-top: 59px;
-display: flex;
-font-size: 14px;
-margin: -13px;
-& > * {
- margin: 13px;
- }
-`
-const Logo = styled.img`
-margin-top: 32px;
-`
+
 
 const Header: React.FC = () =>
     <Root>
-        <Menu>
-            <MenuButton href={ROUTES.FAQ} text="FAQ"/>
-            <MenuButton href={ROUTES.ABOUT} text="About"/>
-            <MenuButton href={ROUTES.BLOG} text="Blog"/>
-            <MenuButton href={ROUTES.DOCS} text="Docs"/>
-            <Button>
-                <Link to={ROUTES.SIGNIN}>Sign In</Link>
-            </Button>
-        </Menu>
-        <Link to={ROUTES.ROOT}><Logo src={logo} alt="Logo"/></Link>
+        <MenuButton href={ROUTES.ADMIN} text="Admin"/>
     </Root>
 
 export default Header;
